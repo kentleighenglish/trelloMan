6764fb36db0d7234f969b0c746a0b8f781b988c3
Kentleigh English <kentleigh.english@bravissimo.com>
2019-06-20 16:54:23 +0100

    Add new changes for handling params

--- msglib/index.js
+++ msglib/index.js
@@ -6,14 +6,8 @@ const regexEscape = string => string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 
 const allCommands = [].concat(msgConfig.commands, msgConfig.boardCommands);
 
-const sendFormatted = (channel, content, type) => {	
-	return channel.send('', {
-		embed: {
-			title: type === 'error' ? 'Error' : 'TrelloMan',
-			description: content,
-			color: type === 'error' ? 16711680 : 2671515,
-		}
-	});
+const sendMessage = async (channel) => {
+	
 }
 
 const onMessage = async ({ member, content, channel, author, ...msg }) => {
@@ -24,8 +18,14 @@ const onMessage = async ({ member, content, channel, author, ...msg }) => {
 			const commandData = await processContent(content);
 			const channelData = await processChannel(channel);
 			
-			if (msgConfig.boardCommands.includes(commandData.command)) {
-				
+			if (msgConfig.boardCommands.includes(commandData.command) && !channelData.board) {
+				throw new Error(messages.noBoard);
+			}
+			
+			const result = await runCommand(commandData, channelData);
+			
+			if (result) {
+				msg.delete();
 			}
 		} catch(e) {
 			debugErr(e);
@@ -34,14 +34,14 @@ const onMessage = async ({ member, content, channel, author, ...msg }) => {
 	}
 }
 
-const commandRegex = new RegExp('\\".+\\"|[^\\s]+', 'g');
+const commandRegex = new RegExp('(?:\")[^\"]*(?:\")|[^\\s]+', 'g');
 
 const processContent = async (content) => {
 	const prefix = regexEscape(msgConfig.prefix);
 	content = content.replace(new RegExp(`^${prefix}`), '');
 	
 	const matches = content.match(commandRegex);
-	
+	console.log(matches);
 	if (!matches.length) {
 		throw new Error(messages.invalidFormat);
 	}
@@ -52,14 +52,42 @@ const processContent = async (content) => {
 	
 	return {
 		command: matches[0],
-		params: matches.splice(1, matches.length)
+		params: matches.splice(1, matches.length).map(param => param.replace(/\"/g, ''))
 	}
 }
 
 const processChannel = async (channel) => {
 	// connect to DB and get channel data
+	
+	return {
+		board: null
+	}
+}
+
+const runCommand = async (command, channel) => {
+	debug(`Running the "${command.command}" command`);
+	switch(command.command) {
+		case 'addBoard':
+			
+		break;
+		default:
+			debug(`${command.command} isn't configured with this bot, but is registered as valid.`);
+			throw new Error(messages.unknownError);
+		break;
+	}
+}
+
+const sendFormatted = (channel, content, type) => {	
+	return channel.send('', {
+		embed: {
+			title: type === 'error' ? 'Error' : 'TrelloMan',
+			description: content,
+			color: type === 'error' ? 16711680 : 2671515,
+		}
+	});
 }
 
 module.exports = {
-	onMessage
+	onMessage,
+	sendMessage
 }
