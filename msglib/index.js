3f9d2fd960659f89aa854b0fb10a820180264184
Kentleigh English <kentleigh.english@bravissimo.com>
2019-06-21 11:47:32 +0100

    Add help messages

--- msglib/index.js
+++ msglib/index.js
@@ -1,8 +1,10 @@
 const debug = require('debug')('app:msglib');
 const debugErr = require('debug')('app:error');
-const { msg: msgConfig, messages } = require('config');
+const { msg: msgConfig, messages, helpMessages } = require('config');
 
 const regexEscape = string => string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+
+const hexToDecimal = hex => parseInt(hex.replace(/#/g, ''), 16);
 
 const allCommands = [].concat(msgConfig.commands, msgConfig.boardCommands);
 
@@ -10,7 +12,9 @@ const sendMessage = async (channel) => {
 	
 }
 
-const onMessage = async ({ member, content, channel, author, ...msg }) => {
+const onMessage = async (msg) => {
+	const { member, content, channel, author } = msg;
+
 	if (content.indexOf(msgConfig.prefix+' ') === 0 && member.hasPermission('ADMINISTRATOR')) {
 		debug(`Received command from @${author.username}`);
 		
@@ -22,9 +26,10 @@ const onMessage = async ({ member, content, channel, author, ...msg }) => {
 				throw new Error(messages.noBoard);
 			}
 			
-			const result = await runCommand(commandData, channelData);
+			const result = await runCommand(commandData, channelData, channel);
 			
 			if (result) {
+				console.log(msg);
 				msg.delete();
 			}
 		} catch(e) {
@@ -41,7 +46,7 @@ const processContent = async (content) => {
 	content = content.replace(new RegExp(`^${prefix}`), '');
 	
 	const matches = content.match(commandRegex);
-	console.log(matches);
+
 	if (!matches.length) {
 		throw new Error(messages.invalidFormat);
 	}
@@ -64,17 +69,31 @@ const processChannel = async (channel) => {
 	}
 }
 
-const runCommand = async (command, channel) => {
-	debug(`Running the "${command.command}" command`);
-	switch(command.command) {
+const runCommand = async (commandData, channelData, channel) => {
+	debug(`Running the "${commandData.command}" command`);
+	switch(commandData.command) {
 		case 'addBoard':
 			
 		break;
+		case 'help':
+			if (commandData.params.length) {
+				const param = commandData.params[0];
+				if (!helpMessages.hasOwnProperty(param)) {
+					throw new Error(messages.helpInvalid);
+				} else {
+					await sendFormatted(channel, helpMessages[param]);
+				}
+			} else {
+				await sendFormatted(channel, 'generic help');
+			}
+		break;
 		default:
-			debug(`${command.command} isn't configured with this bot, but is registered as valid.`);
+			debug(`${commandData.command} isn't configured with this bot, but is registered as valid.`);
 			throw new Error(messages.unknownError);
 		break;
 	}
+	
+	return true;
 }
 
 const sendFormatted = (channel, content, type) => {	
@@ -82,7 +101,7 @@ const sendFormatted = (channel, content, type) => {
 		embed: {
 			title: type === 'error' ? 'Error' : 'TrelloMan',
 			description: content,
-			color: type === 'error' ? 16711680 : 2671515,
+			color: type === 'error' ? hexToDecimal(msgConfig.colours.error) : hexToDecimal(msgConfig.colours.main),
 		}
 	});
 }
