ecbd62e65414375ed8bea908a21be2b312a856be
Kentleigh English <kentleigh@ikengainnovations.com>
2019-06-26 12:04:46 +0000

    Add views

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const config = require('config');
 const debug = require('debug')('app:server');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.resolve('static')));
+
 app.get('/', (request, response) => {
-	
+	response.sendFile(path.resolve('views', 'index.html'));
 });
 
 app.listen(config.port, () => {
